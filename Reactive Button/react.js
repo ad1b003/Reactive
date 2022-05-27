a0b91db79947a8d53ff96b494b1db2d299db343e
ad1b003 <76702138+ad1b003@users.noreply.github.com>
2022-05-28 02:46:07 +0600

    Delete Reactive Button directory

--- Reactive Button/react.js
+++
@@ -1,8 +0,0 @@
-let count = 0;
-const button = document.querySelector('button');
-const text = document.getElementById('text');
-button.addEventListener('click', () => {
-  ++count;
-  text.innerText = "You've clicked";
-  button.innerText = `${count} ${count > 1 ? 'times' : 'time'}`;
-});
